e33aeff841482090e4778f80e662dd2e6e6cdfae
Charlie Wang <charliewang324@gmail.com>
2023-02-09 16:27:06 +0800

    ADD: allSettled

--- MyPromise.js
+++ MyPromise.js
@@ -154,6 +154,32 @@ class MyPromise {
       }
     });
   }
+
+  static allSettled(promises) {
+    const results = [];
+    let completedPromises = 0;
+
+    return new MyPromise((resolve, reject) => {
+      for (let i = 0; i < promises.length; i++) {
+        const promise = promises[i];
+
+        promise
+          .then((value) => {
+            results[i] = { status: STATE.FULFILLED, value }
+          })
+          .catch((reason) => {
+            results[i] = { status: STATE.REJECTED, reason }
+          })
+          .finally(() => {
+            completedPromises += 1;
+
+            if (completedPromises === promises.length) {
+              resolve(results);
+            }
+          });
+      }
+    });
+  }
 }
 
 class UncaughtPromiseError extends Error {
